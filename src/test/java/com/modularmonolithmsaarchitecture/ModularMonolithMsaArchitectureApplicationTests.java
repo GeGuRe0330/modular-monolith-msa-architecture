8144af1a9e722a8cfa4332f7c9af3e09f3e3bf24
Woojin CHO <littletree0330@gmail.com>
2026-07-09 13:16:26 +0900

    🎉 Initialize project

---
+++ src/test/java/com/modularmonolithmsaarchitecture/ModularMonolithMsaArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modularmonolithmsaarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularMonolithMsaArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
